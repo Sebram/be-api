defaa5056a284f639ba2c1621d79452a08cd59b8
Sebram <skonemailbox@gmail.com>
2018-08-06 00:26:04 +0200

    Adding Class ArticleListener implements EventSubscriberInterface in App\Api\Serializer\Listener

---
+++ src/Api/Serializer/Listener/ArticleListener.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Api\Serializer\Listener;
+
+use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+
+Class ArticleListener implements EventSubscriberInterface{
+	
+	public static function getSubscribedEvents() {
+		return [
+			[
+				'event' => Events::POST_SERIALIZE,
+				'format' => 'json',
+				'class' => 'App\Api\Entity\Article',
+				'method' => 'onPostSerialize',
+
+			]
+		];
+	}
+
+	public static function onPostSerialize(ObjectEvent $event) {
+	 	
+	 	$date = new \Datetime();
+	 	
+	 	$event->getVisitor()->addData('serialized_at', $date->format('l jS \of F Y h:s:i'));
+
+	}
+}
